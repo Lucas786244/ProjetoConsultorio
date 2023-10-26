dc6cc3b0163e041bd021d984cb9ae4193d2d4e2a
lucasoliver <141953292+Lucas786244@users.noreply.github.com>
2023-10-26 16:54:47 -0300

    Initial commit

---
+++ ProjetoConsultorio/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoConsultorio.Models;
+
+namespace ProjetoConsultorio.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext (DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoConsultorio.Models.Consulta> Consulta { get; set; } = default!;
+    }
+}
---
+++ ProjetoConsultorio/Models/Consulta.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoConsultorio.Models
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNasc { get; set; }
+        public string Convenio { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
